704bf23c709d9861bf46b421191440c520c4ce92
OmLifeCC <nyraoswal@gmail.com>
2026-07-04 13:26:33 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer, shell } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  minimize:      () => ipcRenderer.send('win:minimize'),
-  maximize:      () => ipcRenderer.send('win:maximize'),
-  close:         () => ipcRenderer.send('win:close'),
-  openExternal:  (url) => shell.openExternal(url),
-});
